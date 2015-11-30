2af2ad4d217ea97e0c32e44c1e5307a260400ec1
Luis Medina <lu4nm3@gmail.com>
2015-11-30 11:34:13 -0800

    Update filter.py

--- filter_plugins/filter.py
+++ filter_plugins/filter.py
@@ -1,26 +1,36 @@
-# import re
-# from jinja2 import attr
-#
-# __author__ = 'luis'
-#
-# class FilterModule(object):
-#     def filters(self):
-#         return {
-#             'luis': self.luis,
-#         }
-#
-#     def luis(self, check, at, checkin):
-#         if check.attr(at) in checkin:
-#             return True
-#         else:
-#             return False
+__author__ = 'luis@escala.io'
 
+class FilterModule(object):
+    def filters(self):
+        return {
+            'attrinlist': self.attr_in_list,
+        }
 
+    # Returns a new seq of items whose attributes matched any of the elements in the match_list.
+    def attr_in_list(self, seq, path, match_list):
+        try:
+            path = str(path) # Path must be a string.
+        except UnicodeError:
+            raise
+        else:
+            for item in seq:
+                if self.matches_any_in_list(path, item, match_list):
+                    yield item
 
-# from jinja2 import Environment
-# def luis(value, list):
-#     if value in list:
-#         return True
-#     else:
-#         return False
-# environment.tests['luis'] = luis
+    def matches_any_in_list(self, path, mapping, match_list):
+        try:
+            value = self.getattrd(mapping, path) 
+        except AttributeError:
+            raise
+        else:
+            if value in match_list:
+                return True
+            else:
+                return False
+
+    # Uses reduce to get to attributes separated by a "." in a nested dictionary.
+    def getattrd(self, obj, name):
+        try:
+            return reduce(dict.get, name.split('.'), obj)
+        except AttributeError, e:
+            raise
